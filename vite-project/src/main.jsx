cc950ed55338a68b8c58e8195ab7cb88b2300495
[Talha] <[dev.talha053@gmail.com ]>
2024-07-11 14:51:08 +0500

    Implement Lazy Loading in nav bar

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,49 +1,59 @@
-import React from 'react'
+import React, { Suspense }  from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Header from "./components/header";
 import Layout from "./components/layout"
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Contactus from "./components/contactus";
-import Menu from "./components/menu";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./components/home";
-import Cart from "./components/Cart"
-import { store } from './redux/store/store'
-import { Provider } from 'react-redux'
-import Aboutus from "./components/aboutus";
-// import '@shopify/polaris/build/esm/styles.css';
-import AdminLogin from "./components/adminLogin";
+import {store} from './redux/store/store'
+import {Provider} from 'react-redux'
+const Contactus = React.lazy(() => import('./components/contactus'));
+const Aboutus = React.lazy(() => import('./components/aboutus'));
+const  AdminLogin = React.lazy(() => import('./components/adminLogin'));
+const Menu = React.lazy(() => import('./components/menu'));
+const Cart = React.lazy(() => import('./components/Cart'));
 const router = createBrowserRouter(
     [
         {
             path: "/",
-            element: <Layout />,
+            element: <Layout/>,
             children: [
                 {
                     index: true, // This makes it the default child route for "/"
-                    element: <Home />
+                    element: <Home/>
                 },
                 {
                     path: "contactus",
-                    element: <Contactus />
+                    element: ( <Suspense fallback={<div>Loading...</div>}>
+                        <Contactus/>
+                    </Suspense> )
                 },
                 {
                     path: "menu",
-                    element: <Menu />
+                    element: ( <Suspense fallback={<div>Loading...</div>}>
+                        <Menu/>
+                    </Suspense> )
                 },
                 {
                     path: "Cart",
-                    element: <Cart />
+                    element: ( <Suspense fallback={<div>Loading...</div>}>
+                        <Cart/>
+                    </Suspense> )
                 },
                 {
                     path: "aboutus",
-                    element: <Aboutus />
+                    element:( <Suspense fallback={<div>Loading...</div>}>
+                        <Aboutus/>
+                    </Suspense> )
+
                 },
-                // {
-                //     path: "adminLogin",
-                //     element: <AdminLogin />
-                // }
+                {
+                    path: "adminLogin",
+                    element: ( <Suspense fallback={<div>Loading...</div>}>
+                        <AdminLogin/>
+                    </Suspense> )
+                }
             ]
         }
     ]
@@ -52,7 +62,7 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-        <RouterProvider router={router} />
+            <RouterProvider router={router}/>
         </Provider>
     </React.StrictMode>
 )
